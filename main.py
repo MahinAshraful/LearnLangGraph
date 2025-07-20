68ba6d42dfc9a21c5ebeef2f115406748a118d74
MahinAshraful <150500453+MahinAshraful@users.noreply.github.com>
2025-07-19 23:53:48 -0400

    initial agent

---
+++ main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from typing import Annotated, Literal
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter your message: ")
+state = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+
+print(state["messages"][-1].content)
